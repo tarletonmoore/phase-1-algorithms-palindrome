4af46843b42db31d3822e2aec1de302dac94b072
John Tarleton Moore <tarletonmoore@gmail.com>
2022-04-08 16:10:38 -0400

    commit tree aca1c24deb4fc2aeed6fdcde3aaf86df88c314fb

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (word === word.split(``).reverse().join(``)) {
+    return true
+  }
+  else { return false }
 }
 
 /* 
   Add your pseudocode here
+  need to make conditions
+  have word be read backwords and if so returned true
+  if it isn't return false
 */
 
 /*
   Add written explanation of your solution here
+  using an if statement to take a string and say if this word is the same forward and backwards, then return true. If the string is not the same, then return false. It is splitting each letter of the string into part of an array, then it is reverseing the letters and joining them back together into a string. The if statement can then read if the string is equal to itself, then return true.
 */
 
 // You can run `node index.js` to view these console logs
